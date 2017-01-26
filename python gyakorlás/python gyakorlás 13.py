9c59d64a1cc740c6a13d0c68d58c5797df8c8e25
Nedbalekbalazs <e16.nedbalek.balazs@palfy-vizugyi.hu>
2017-01-26 18:29:48 +0100

    python gyakorlás kódok
    ezek a python idomár gyakorlásaim (Nedbalek Balázs 9.e)

---
+++ python gyakorlás/python gyakorlás 13.py
@@ -0,0 +1,4 @@
+majmok = 1
+while (majmok/8)**2 + 12 !=majmok:
+    majmok += 1
+print(majmok, 'a megoldás')    
---
+++ python gyakorlás/python gyakorlás 7.py
@@ -0,0 +1,7 @@
+kor = int(input('Hány éves vagy ?'))
+if kor < 6:
+    print('Nézz prioskát.')
+elif 6 <= kor <= 16:
+    print('Nézz Zootropoliszt.')
+elif kor > 16:
+    print('Nézzél bármi mást mert már nagy vagy.')
